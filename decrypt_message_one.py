64c61e3ee4d097733593131fdf24e5c7a3f5cf93
typow10 <tyler.power@uconn.edu>
2023-09-11 13:51:14 -0400

    Adds a python script that successfully decodes encrypted message 2

--- decrypt_message_one.py
+++ decrypt_message_one.py
@@ -41,3 +41,11 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write code below
+
+solution = ''
+
+for char in encrypted_message:
+    for x in cipher:
+        if char == cipher[x]: solution += x
+
+print(solution)
--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,22 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+sollist = list()
+for z in range(len(encrypted_message)):
+    sollist.append('')
+count = 1
+solution = ''
+for x in range(len(encrypted_message)):
+        if (count % 2) == 0:
+            first = encrypted_message[len(encrypted_message) - 1 - x]
+            second = encrypted_message[x]
+        else:
+            first = encrypted_message[x]
+            second = encrypted_message[len(encrypted_message) - 1 - x]
+        count += 1
+        if x >= (len(encrypted_message) - x): break
+        sollist[x], sollist[len(sollist) - x - 1] = first, second
+for letter in sollist:
+    solution += letter
+print(solution)
+
